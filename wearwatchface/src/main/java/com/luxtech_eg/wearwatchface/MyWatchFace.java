30f0bed7836a1db463c39a182b8cfc289048e886
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-11 18:24:03 +0200

    seconds from text

--- wearwatchface/src/main/java/com/luxtech_eg/wearwatchface/MyWatchFace.java
+++ wearwatchface/src/main/java/com/luxtech_eg/wearwatchface/MyWatchFace.java
@@ -260,11 +260,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
             long now = System.currentTimeMillis();
             mCalendar.setTimeInMillis(now);
 
-            String text = mAmbient
-                    ? String.format("%d:%02d", mCalendar.get(Calendar.HOUR),
-                    mCalendar.get(Calendar.MINUTE))
-                    : String.format("%d:%02d:%02d", mCalendar.get(Calendar.HOUR),
-                    mCalendar.get(Calendar.MINUTE), mCalendar.get(Calendar.SECOND));
+            String text = String.format("%d:%02d", mCalendar.get(Calendar.HOUR), mCalendar.get(Calendar.MINUTE));
             canvas.drawText(text, mXOffset, mYOffset, mTextPaint);
         }
 
