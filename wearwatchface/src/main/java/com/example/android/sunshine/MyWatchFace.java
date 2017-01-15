a4fc27f4ad98a4969dad84a0d849a7f213097096
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2017-01-15 07:10:48 +0200

    changing package name to com.example.android.sunshine

--- wearwatchface/src/main/java/com/example/android/sunshine/MyWatchFace.java
+++ wearwatchface/src/main/java/com/example/android/sunshine/MyWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.luxtech_eg.wearwatchface;
+package com.example.android.sunshine;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
